027c1a475fc42642f1c8bda13f65719a7cd09b7f
김도영 <zz01064842675@gmail.com>
2026-01-20 09:44:09 +0900

    feat: init

---
+++ src/test/java/com/zeroq/back/ZeroqBackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeroq.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroqBackServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
